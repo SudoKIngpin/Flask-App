f8c2b33eb483b0b980c087a32bfe3358cae34ee4
sudokingpin <wd51886@gmail.com>
2023-03-13 18:20:12 +0530

    Fonts , css and pdfs added, meta tags !

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template
+from flask import Flask , render_template , redirect 
 app=Flask(__name__)
 
 @app.route('/')
@@ -7,9 +7,9 @@ def h():
 
 @app.route("/s1")
 def s1():
-    return render_template("s1.html")
+        return redirect("https://drive.google.com/drive/folders/1Mn-p3Xtb0knsjWKU7n8h2pCqam3r_K3C?usp=sharing", code=302)
 
 @app.route("/s2")
 def s2():
-    return render_template("s2.html")
-app.run(debug=True)
+    return redirect("https://drive.google.com/drive/folders/1ISC0C_-ub0rEknT9DcG0Rgu-keUPJPVh?usp=sharing",code=302)
+app.run(host="0.0.0.0")
