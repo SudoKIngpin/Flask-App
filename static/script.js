6e7e32b5c264c35b5da62a89c138618c3b9efa83
sudoKIngpin <wd51886@gmail.com>
2024-12-02 18:13:20 +0530

    Theme&Design updated

---
+++ static/script.js
@@ -0,0 +1,23 @@
+const textElement = document.getElementById("text");
+const message = "Welcome  JUNIORS 👋 ! ";
+let index = 0;
+let isDeleting = false;
+
+function typeEffect() {
+    if (isDeleting) {
+        textElement.textContent = message.substring(0, index--);
+    } else {
+        textElement.textContent = message.substring(0, index++);
+    }
+
+    if (!isDeleting && index === message.length) {
+        setTimeout(() => isDeleting = true, 1000); // Pause before deleting
+    } else if (isDeleting && index === 0) {
+        isDeleting = false;
+    }
+
+    setTimeout(typeEffect, isDeleting ? 160: 185); // Adjust typing and deleting speed
+}
+
+// Start the typing effect
+typeEffect();
