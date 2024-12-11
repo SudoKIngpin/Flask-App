0e2bf45296136a70adbd3177ada86d9c0cea2529
sudoKIngpin <wd51886@gmail.com>
2024-12-11 20:27:16 +0530

    sitemap updated

--- app.py
+++ app.py
@@ -89,13 +89,43 @@ def robots_txt():
 @app.route('/sitemap.xml')
 def sitemap():
     sitemap_xml = '''<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-        <url>
-            <loc>https://siet-exams.onrender.com/</loc>
-            <changefreq>daily</changefreq>
-            <priority>1.0</priority>
-        </url>
-    </urlset>'''
+<urlset
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+
+<url>
+  <loc>https://siet-exams.onrender.com/</loc>
+  <lastmod>2024-12-11T14:52:24+00:00</lastmod>
+  <priority>1.00</priority>
+</url>
+<url>
+  <loc>https://siet-exams.onrender.com/feedback</loc>
+  <lastmod>2024-12-11T14:52:24+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>https://siet-exams.onrender.com/1st</loc>
+  <lastmod>2024-12-11T14:52:24+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>https://siet-exams.onrender.com/2nd</loc>
+  <lastmod>2024-12-11T14:52:24+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>https://siet-exams.onrender.com/3rd</loc>
+  <lastmod>2024-12-11T14:52:24+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>https://siet-exams.onrender.com/4th</loc>
+  <lastmod>2024-12-11T14:52:24+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+</urlset>'''
     response = Response(sitemap_xml, mimetype='application/xml')
     return response
 
