5d8e718494c7a2899111d02291079041fad99afd
Harsh Vishwakarma <68921425+SudoKIngpin@users.noreply.github.com>
2025-09-29 22:52:57 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -26,18 +26,18 @@ typeEffect();
 
 
 // Show the popup after the page loads
-window.onload = function() {
-    setTimeout(function() {
-        document.getElementById("exam-popup").style.display = "block";
-        document.getElementById("popup-overlay").style.display = "block";
-    }, 2500); // Popup appears 1 second after page load
-};
+// window.onload = function() {
+//     setTimeout(function() {
+//         document.getElementById("exam-popup").style.display = "block";
+//         document.getElementById("popup-overlay").style.display = "block";
+//     }, 2500); // Popup appears 1 second after page load
+// };
 
 // Close the popup when the close button is clicked
-function closePopup() {
-    document.getElementById("exam-popup").style.display = "none";
-    document.getElementById("popup-overlay").style.display = "none";
-}
+// function closePopup() {
+//     document.getElementById("exam-popup").style.display = "none";
+//     document.getElementById("popup-overlay").style.display = "none";
+// }
 
 
 
